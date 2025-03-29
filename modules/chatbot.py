e6e664703162cffe873b6912b1ff6c01aab4451b
DataChicGirl <157007903+Sapphire-Oshodi@users.noreply.github.com>
2025-03-29 19:52:28 +0100

    Update chatbot.py

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -55,7 +55,7 @@ def main():
     
     # Display the main image at the top
     try:
-        st.image("assets\image9.jpg", width=800)
+        st.image("assets\Untitled_design.png", width=800)
     except FileNotFoundError:
         st.warning("Main image not found. Please check the path.")
     
