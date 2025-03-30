752075d1baf33710e2e1051427acd89c0cc7824f
DataChicGirl <157007903+Sapphire-Oshodi@users.noreply.github.com>
2025-03-30 08:01:58 +0100

    Update ada.py

--- modules/ada.py
+++ modules/ada.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain-community.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import HumanMessage
 
